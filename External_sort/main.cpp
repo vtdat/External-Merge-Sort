c32938f297effd6017c419fec34083c4a1359335
Dat Vu <tuandatk25a@gmail.com>
2018-08-13 11:58:29 +0800

    complete external sort

--- External_sort/main.cpp
+++ External_sort/main.cpp
@@ -7,82 +7,142 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <iterator>
+#include <queue>
 
-// Queue node to be put in priority queue
-struct QueueNode {
-    // line stored
-    char* element;
-    // index of file
-    int index;
-};
-
-// comparision
-struct comp {
-    bool operator ()(const QueueNode a, const QueueNode b) {
-        return strcmp(a.element, b.element);
-    }
-};
-
-//  open file
-FILE* openFile(char* fileName, char mode) {
-    FILE* fp = fopen(fileName, &mode);
-    if (fp == NULL) {
-        perror("ERROR while opening the file");
-        exit(EXIT_FAILURE);
-    }
-    return fp;
-}
-//  split file
-void splitFile(char* inFile, int ramSize) {
-    FILE* in = openFile(inFile, 'r');
-    char fileName[5];
-    
-    //  initialize pointer to output files
-    std::vector<FILE*> out (1, 0);
-
-    //  initialize buffer
-    char* buffer = (char*)malloc(ramSize * sizeof(char*));
-    if (buffer == NULL) {
-        perror("ERROR while malloc buffer");
-        exit(EXIT_FAILURE);
-    }
-    //  track number of file created
-    int outFile = 0;
-    
-    //  split big file to sorted chunks
-    size_t nread;
-    while (((nread = fread(buffer, 1, ramSize, in)) > 0)) {
-        
-        //  check last character for complete line
-        int offsetGap = 0;
-        while (buffer[nread - offsetGap - 1] != '\n' && buffer[nread - offsetGap - 1] != EOF)
-            offsetGap++;
-        buffer[nread-offsetGap] = '\0';
-        fseek(in, -offsetGap, SEEK_CUR);
-        
-        //  create output file and write to file
-        out.resize(outFile + 1);
-        
-        //  convert outFile to fileName
-        snprintf(fileName, sizeof(fileName), "%d", outFile);
-        out[outFile] = openFile(fileName, 'w');
-        fwrite(buffer, 1, nread, out[outFile]);
-        outFile++;
-        
-        //  TO DO: sort lines in file
-     
+using namespace std;
+class External_Sort
+{
+    private:
+    int numChunks = 0;
+    string inFileName = "";
+    long ramSize = 0;
+    string outFileName = "";
+    string path = "/Users/Dat/workspace/External_sort/External_sort/";
+    //  constructor
+    public: External_Sort(string inFile, string outFile, long size){
+        inFileName = inFile;
+        outFileName = outFile;
+        ramSize = size;
     };
     
-    //  close all file descriptor
-    for (int i = 0; i < outFile; i++) {
-        fclose(out[i]);
+    // Queue node to be put in priority queue
+    struct QueueNode {
+        // line stored
+        string element;
+        // index of file
+        int index;
+    };
+
+    // comparision
+    struct comp {
+        bool operator ()(const QueueNode a, const QueueNode b) {
+            return (a.element).compare(b.element);
+        }
+    };
+
+    //  split file
+    public: void splitFile() {
+        ifstream in;
+        in.open(inFileName);
+        string tempFile;
+        
+        //  track number of file created
+        int numOutFile = 0;
+
+        //  split big file to sorted chunks
+        while (!in.eof()) {
+            //  initialize buffer size
+            int buffSize = 0;
+            
+            //  initialize vector to store line loaded
+            vector<string> lines;
+            string line;
+            while (getline(in, line) && buffSize < ramSize) {
+                lines.push_back(line+"\n");
+                buffSize += (int)line.length();
+            };
+            
+            //  sort vector of string
+            sort(lines.begin(), lines.end());
+            
+            //  convert outFile to fileName
+            tempFile = to_string(numOutFile);
+            //  copy buffer to chunk
+            ofstream outStream (path + tempFile + ".txt");
+            ostream_iterator<string> output_iterator(outStream);
+            copy(lines.begin(), lines.end(), output_iterator);
+            
+            //  close file descriptor
+            outStream.close();
+            numOutFile++;
+        };
+        in.close();
+        numChunks = numOutFile++;
     }
-    fclose(in);
-}
-// merge file using merge K-sorted array
+    
+    // merge file using merge K-sorted array
+    public: void mergeFiles () {
+        ifstream inFiles[numChunks];
+        string inName;
+        
+        //  open file to stream
+        for (int i = 0; i < numChunks; i++) {
+            inName = path + to_string(i) + ".txt";
+            inFiles[i].open(inName);
+        }
+        
+        
+        //  create output stream
+        ofstream out(outFileName);
+        
+        //  create queue
+        QueueNode nodes[numChunks];
+        
+        //  create priority queue
+        priority_queue<QueueNode, vector<QueueNode>, comp> pq;
+        
+        //  read data to queue
+        for (int i = 0; i < numChunks; i++) {
+            if (!getline(inFiles[i], nodes[i].element))
+                break;
+            
+            nodes[i].index = i;
+            pq.push(nodes[i]);
+        }
+        
+        //  count number of file reaching EOF
+        int count = 0;
+        
+        while (count != numChunks) {
+            
+            //  put minimun element from priority queue to output
+            QueueNode top = pq.top();
+            pq.pop();
+            out << top.element << "\n";
+            
+            //  put next element of input file containing last popped element
+            if (!getline(inFiles[top.index], top.element)) {
+                count++;
+            } else {
+                pq.push(top);
+            }
+        };
+        
+        //  close all input and output streams
+        for (int i = 0; i < numChunks; i++)
+            inFiles[i].close();
+        out.close();
+    }
+    
+};
 
 int main(int argc, const char * argv[]) {
-    splitFile("/Users/Dat/workspace/External_sort/External_sort/english.txt", 100000);
+    External_Sort sort("/Users/Dat/workspace/External_sort/External_sort/english.txt", "/Users/Dat/workspace/External_sort/External_sort/result.txt", 200000);
+    sort.splitFile();
+    sort.mergeFiles();
     return 0;
 }
 
